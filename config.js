5acc50d284ba241b0b4f4801fd89f3a1c166f163
Amin-Farina <58991019+amin-farina@users.noreply.github.com>
2023-04-21 20:47:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export default {
+    SERVER_PORT: 3000,
+    DB_NAME:   "padronDB" ,
+    DB_USER:   "admin" ,
+    DB_PASSWORD:   "Pampero7423.-" ,
+    DB_PORT:  3306,
+    DB_HOST: "database-1.ctf2icg9iuk0.us-east-2.rds.amazonaws.com",
+    DB_DIALECT: "mysql"
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import config from './config.js';
+import db from './db.js';
+import apiRoutes from './Routes/index.js';
+import encuestaDB from './Models/encuesta.js';
+import cors from 'cors';
+
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use("/api", apiRoutes)
+
+db.authenticate()
+.then(() => {
+    return encuestaDB.sync({ force: true });
+  })
+.then(() => {
+    app.listen (3000);
+    console.log("Server started on port " + config.SERVER_PORT)
+})
+.catch((err) => {
+    console.log("Error starting server: " + err);
+});
