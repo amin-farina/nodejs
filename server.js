2a7d254fda1f67758ba09710a7cd8f0366880c1d
Sebastian Amin Fariña <amiin74@gmail.com>
2023-04-24 00:00:59 -0300

    Nuevos cambios

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json());
 app.use("/api", apiRoutes)
 
-db.authenticate({alter:true})
+db.authenticate()
 .then(() => {
     return encuestaDB.sync({ force: true });
   })
